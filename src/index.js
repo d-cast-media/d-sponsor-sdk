16c0b42ab3a0173ed380205363c783d14fbd9992
Alex Werner <obusco@gmail.com>
2024-03-18 04:59:19 +0100

    feat: provide with base entry points

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as AdSpaceRenderer } from './services/AdSpaceRenderer/AdSpaceRenderer.js';
+export { default as DSponsorNFT } from './services/DSponsorNFT/DSponsorNFT.js';
+export { default as DSponsorAdmin } from './services/DSponsorAdmin/DSponsorAdmin.js';
+export { default as Ad } from './primitives/Ad/Ad.js';
+export { default as Wallet } from './primitives/Wallet/Wallet.js';
+export { default as Token } from './primitives/Token/Token.js';
+export { default as ChainNetwork } from './primitives/ChainNetwork/ChainNetwork.js';
+export { default as DSponsorSDK } from './DSponsorSDK.js';
